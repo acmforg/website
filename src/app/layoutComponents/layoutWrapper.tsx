fbca61d3b38ec770a0a1b8339b0f1a30f1637690
reedwane <abdulkareemtemmy07@gmail.com>
2023-09-19 18:27:58 +0100

    remove max-width from layout

--- src/app/layoutComponents/layoutWrapper.tsx
+++ src/app/layoutComponents/layoutWrapper.tsx
@@ -3,13 +3,12 @@
 import { ReactNode } from 'react'
 import Header from './header'
 import Footer from './footer'
-import { Maxwidth } from '@/components'
 
 const LayoutWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Header />
-      <Maxwidth tw="min-h-[calc(100vh_-_5rem)]">{children}</Maxwidth>
+      <div tw="min-h-[calc(100vh_-_5rem)]">{children}</div>
       <Footer />
     </>
   )
