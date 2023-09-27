ada55f0354ac931de3844efc7d3cffc312f2b892
reedwane <abdulkareemtemmy07@gmail.com>
2023-09-27 18:45:35 +0100

    attempt fit

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,13 +5,9 @@ import { ReactNode } from 'react'
 import { dehydrate, Hydrate } from '@tanstack/react-query'
 import { fetchEvents } from '@/utils/events'
 
-export default async function EventsWrapper({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function EventsWrapper({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient()
-  await queryClient.prefetchQuery(['events'], fetchEvents)
+  queryClient.prefetchQuery(['events'], fetchEvents)
   const dehydratedState = dehydrate(queryClient)
 
   return <Hydrate state={dehydratedState}>{children}</Hydrate>
