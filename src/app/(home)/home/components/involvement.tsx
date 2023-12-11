57f2d28904ae00a5b2b59862f0e2a0ad9f9c7148
reedwane <abdulkareemtemmy07@gmail.com>
2023-12-11 18:04:37 +0100

    add link for becoming member

--- src/app/(home)/home/components/involvement.tsx
+++ src/app/(home)/home/components/involvement.tsx
@@ -10,13 +10,13 @@ const Involvement = () => {
 
       <FancyHeader ordinary="Get" colored="Involved" />
 
-      <div tw="flex flex-col gap-10 lg:(flex-row gap-10)">
+      <div tw="flex flex-col gap-10 lg:(flex-row flex-wrap gap-10 justify-center)">
         {involvements.map(involvement => (
           <div
             key={involvement.title}
             css={[
               { background: `url(${involvement.bg})` },
-              tw`bg-primary rounded-[1rem] px-6 py-[4rem] text-white text-center bg-center bg-cover flex flex-col items-center gap-6 flex-1`,
+              tw`bg-primary rounded-[1rem] px-6 py-[4rem] text-white text-center bg-center bg-cover flex flex-col items-center gap-6 flex-1 lg:(min-w-[30rem] max-w-[35rem])`,
             ]}
           >
             <Heading $variant="h4" as={'h4'}>
@@ -25,7 +25,12 @@ const Involvement = () => {
 
             <Paragraph>{involvement.desc}</Paragraph>
 
-            <Button as={'a'} href={involvement.ctaUrl} tw="mt-4">
+            <Button
+              as={'a'}
+              href={involvement.ctaUrl}
+              target={involvement?.target ?? ''}
+              tw="mt-4"
+            >
               {involvement.cta}
             </Button>
           </div>
