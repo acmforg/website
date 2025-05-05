459db239345f8a80c49a530355f2eb0922e6b0cc
reedwane <abdulkareemtemmy07@gmail.com>
2025-05-05 22:19:27 +0100

    init analytics

--- src/app/(home)/donate/page.tsx
+++ src/app/(home)/donate/page.tsx
@@ -11,32 +11,32 @@ const QR_URL = 'https://acmfng.org?referrer=banner-qr-code-may-25'
 const DonatePage = () => {
   const qrRef = useRef<SVGSVGElement>(null)
 
-  const downloadQRCode = () => {
-    const svg = qrRef.current
-    if (!svg) return
-    const serializer = new XMLSerializer()
-    const svgString = serializer.serializeToString(svg)
-    const canvas = document.createElement('canvas')
-    const img = new window.Image()
-    const size = 1000 // High-res size
-    canvas.width = size
-    canvas.height = size
-    img.onload = function () {
-      const ctx = canvas.getContext('2d')
-      ctx?.fillRect(0, 0, size, size)
-      ctx?.drawImage(img, 0, 0, size, size)
-      const pngUrl = canvas.toDataURL('image/png')
-      const downloadLink = document.createElement('a')
-      downloadLink.href = pngUrl
-      downloadLink.download = 'acmf-donate-qrcode.png'
-      document.body.appendChild(downloadLink)
-      downloadLink.click()
-      document.body.removeChild(downloadLink)
-    }
-    img.src =
-      'data:image/svg+xml;base64,' +
-      window.btoa(unescape(encodeURIComponent(svgString)))
-  }
+  // const downloadQRCode = () => {
+  //   const svg = qrRef.current
+  //   if (!svg) return
+  //   const serializer = new XMLSerializer()
+  //   const svgString = serializer.serializeToString(svg)
+  //   const canvas = document.createElement('canvas')
+  //   const img = new window.Image()
+  //   const size = 1000 // High-res size
+  //   canvas.width = size
+  //   canvas.height = size
+  //   img.onload = function () {
+  //     const ctx = canvas.getContext('2d')
+  //     ctx?.fillRect(0, 0, size, size)
+  //     ctx?.drawImage(img, 0, 0, size, size)
+  //     const pngUrl = canvas.toDataURL('image/png')
+  //     const downloadLink = document.createElement('a')
+  //     downloadLink.href = pngUrl
+  //     downloadLink.download = 'acmf-donate-qrcode.png'
+  //     document.body.appendChild(downloadLink)
+  //     downloadLink.click()
+  //     document.body.removeChild(downloadLink)
+  //   }
+  //   img.src =
+  //     'data:image/svg+xml;base64,' +
+  //     window.btoa(unescape(encodeURIComponent(svgString)))
+  // }
 
   return (
     <main tw="bg-white">
